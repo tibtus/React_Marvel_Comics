2e70f6e506b1cd102d6c5d9436a23ee4c61d98ef
TibTus <tibtus@gmail.com>
2022-03-13 16:51:05 +0200

    #project_staring_files

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# React_Marvel_Comics
-React_Marvel_Comics
+## Available Scripts
+
+Use the package manager [WebPack](https://webpack.js.org) to install.
+```bash
+npm i 
+```
+
+
+
+In the project directory, you can run:
+
+```bash
+npm start 
+```
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/services/MarvelService.js
@@ -0,0 +1,18 @@
+
+class MarvelService {
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResource('https://gateway.marvel.com:443/v1/public/characters?apikey=28c0df928e5dcc550424c8b861f40578');
+    }
+}
+
+export default MarvelService;
