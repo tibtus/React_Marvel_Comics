9fe387941e411ceb3af4d618ba9b7060c5c39c57
TibTus <tibtus@gmail.com>
2022-03-16 16:43:22 +0200

    #project_Charinfo_mod_1

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -87,10 +87,16 @@ class CharInfo extends Component {
 
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki, comics} = char;
+    
+    let imgStyle = {'objectFit' : 'cover'};
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'contain'};
+    }
+   
     return (
         <>
             <div className="char__basics">
-                    <img src={thumbnail} alt={name}/>
+                    <img src={thumbnail} alt={name} style={imgStyle}/>
                     <div>
                         <div className="char__info-name">{name}</div>
                         <div className="char__btns">
@@ -108,11 +114,14 @@ const View = ({char}) => {
             </div>
             <div className="char__comics">Comics:</div>
             <ul className="char__comics-list">
+                {comics.length > 0 ? null : "Нет описания комиксов"}
                 {
                     comics.map((item, i) => {
-                        return (
+                        // eslint-disable-next-line
+                        if (i > 9) return;
+                        return (                            
                             <li key={i} className="char__comics-item">
-                                {item.name}
+                                {item.name} 
                             </li>
                         )
                     })
